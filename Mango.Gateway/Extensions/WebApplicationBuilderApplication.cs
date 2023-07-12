2a41d381af894ed551c4e0c623438de58abb795f
Alif829 <bsse1120@iit.du.ac.bd>
2023-07-13 00:09:17 +0600

    gateway configured

---
+++ Mango.Gateway/Extensions/WebApplicationBuilderApplication.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Mango.Gateways.Extensions
+{
+    public static class WebApplicationBuilderApplication
+    {
+        public static WebApplicationBuilder AddAppAuthentication(this WebApplicationBuilder builder)
+        {
+            var settingsSections = builder.Configuration.GetSection("ApiSettings");
+
+            var secret = settingsSections.GetValue<string>("Secret");
+            var issuer = settingsSections.GetValue<string>("Issuer");
+            var audience = settingsSections.GetValue<string>("Audience");
+
+            var key = Encoding.ASCII.GetBytes(secret);
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                    ValidateAudience = true
+                };
+            });
+            
+            return builder;
+        }
+    }
+}
